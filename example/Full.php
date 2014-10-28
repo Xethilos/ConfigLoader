368e53218f2820a61c77ca101182f2d14a7f3e9d
Xethilos <xethilos@gmail.com>
2014-10-28 23:57:59 +0100

    Update Full.php

--- example/Full.php
+++ example/Full.php
@@ -27,6 +27,7 @@ $config->add('components', function ($values, $namespace) {
 $config->execute($config::CHECK); // Process
 
 var_dump($config->getParameters('Add')); // Get parameters from add.neon
+
 var_dump($config->getParameters('AddNamespace')); // Get parameters from add.namespace.neon
 var_dump($config->models); // Get models
 var_dump($config->resources); // Get resources
@@ -38,3 +39,29 @@ echo $parameters['limitPerPage'];
 $parameters->change(array(
     'limitPerPage' => 50
 )); // Change limtPerPage and save to file
+
+// DUMPERD VALUES OF $config->data;
+array (7)
+    models => array (1)
+        0 => "Model\Model" (11)
+    routers => array ()
+    resources => array (1)
+        news => "News" (4)
+    privileges => array (1)
+        news => array (3)
+            add => "Add" (3)
+            edit => "Edit" (4)
+            delete => "Delete" (6)
+    menu => array (1)
+        0 => array (2)
+            parent => array (1) [ ... ]
+            childrens => array (2) [ ... ]
+    parameters => array (1)
+        Add => Settings
+            values protected => array (2) [ ... ]
+            filePath protected => ".../config/add.neon" (92)
+            namespace protected => "Add" (3)
+            cache => Cache #6d6c { ... }
+    components => array(1)
+        0 => 'Component\Component'
+// END
